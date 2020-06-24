82cf44460c36dcd2ce626fe138b67be3abb12663
Paulo Castellano <paulofcastellano@gmail.com>
2020-06-24 12:14:32 -0300

    first commit

---
+++ src/Changelogfy.vue
@@ -0,0 +1,59 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: "ChangelogfyVueJS",
+  props: {
+    selector: {
+      type: String,
+      required: true
+    },
+    appId: {
+      type: String,
+      required: true
+    },
+    initBadge: {
+      type: Boolean,
+      required: false,
+      default: true
+    },
+    user: {
+      type: Object,
+      required: false,
+      default: null
+    }
+  },
+  data() {
+    return {};
+  },
+  async mounted() {
+    //Create promise which resolves on script load
+    let promise = new Promise((resolve, reject) => {
+      let script = document.createElement("script");
+      script.src = "https://widget.changelogfy.com/index.js";
+      script.id = "changelogfy-script";
+      script.setAttribute("async", true);
+      document.head.appendChild(script);
+      script.onload = resolve; //Resolve when loaded
+      script.onerror = reject;
+    });
+    await promise; //Await for the script to be resolved
+
+    this.$nextTick(() => {
+      /*global changelogfy*/
+
+      changelogfy.destroy();
+      changelogfy.init({
+        selector: this.selector,
+        app_id: this.appId
+      });
+    });
+  },
+  methods: {
+    open() {},
+    close() {}
+  }
+};
+</script>
---
+++ src/demo.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <a href="#" class="changelogfy-icon" v-on:click="open">
+      Open
+      <Changelogfy app-id="af12f992-0a0c-4cdf-8144-89af158c39e2" selector=".changelogfy-icon" />
+    </a>
+  </div>
+</template>
+
+
+<script>
+import Changelogfy from "./Changelogfy.vue";
+export default {
+  components: {
+    Changelogfy
+  },
+
+  methods: {
+    open() {}
+  }
+};
+</script>
