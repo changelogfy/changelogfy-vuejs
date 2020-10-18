361937eab60c712963691460f8932397def153d5
Paulo Castellano <paulofcastellano@gmail.com>
2020-10-18 16:01:27 -0300

    small improve

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,14 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2020": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:vue/essential"
-    ],
-    "parserOptions": {
-        "ecmaVersion": 11,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "vue"
-    ],
-    "rules": {
-    }
+  env: {
+    browser: true,
+    es2020: true,
+    commonjs: true,
+  },
+  extends: ["eslint:recommended", "plugin:vue/essential"],
+  parserOptions: {
+    ecmaVersion: 11,
+    sourceType: "module",
+  },
+  plugins: ["vue"],
+  rules: {},
 };
